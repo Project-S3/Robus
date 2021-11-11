83593fbe9a2d47e7e8e7a46304ae343b3f7074aa
Jean-Philippe <info@jplemay.com>
2021-11-11 13:51:59 -0500

    Add ball

--- robus.py
+++ robus.py
@@ -1,3 +1,27 @@
+from framer import *
+from car import *
+from ball import *
+
 def main():
     print("hello world")
+
+    car = Car("Car")
+    car.set_location([0, 0, 0])
+    car.set_rotation([np.pi/2, 0, 0])
+    car.velocity = [0, 0, 0]
+    car.acceleration = [0, 20, 0]
+    car.angular_velocity = [0, 0, 3.14/1000]
+
+    ball = Ball("Ball", car)
+    ball.set_location([0, 30, 45])
+    # ball.velocity = [-1, 4, 2]
+    # ball.acceleration = [0.4, 2, 6]
+
+    f = Framer()
+    f.entities = {
+        "car": car,
+        "ball": ball
+    }
+    f.clear_animation()
+    f.play_animation(second=10)
 
