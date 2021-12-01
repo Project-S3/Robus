30186cf3bdc7642e53321494fc5f2c0a43445dd3
Jean-Philippe <info@jplemay.com>
2021-12-01 10:38:08 -0500

    Clean code mef_control

--- mef_control.py
+++ mef_control.py
@@ -5,12 +5,11 @@ import numpy as np
 class State(Enum):
     START = 1
     ACCELERATION = 2
-    TURN_LEFT = 3
-    TURN_RIGHT = 4
-    MAX_SPEED = 5
-    STOP = 6
-    GET_AROUND_OBJECT = 7
-    END = 8
+    TURN = 3
+    MAX_SPEED = 4
+    STOP = 5
+    GET_AROUND_OBJECT = 6
+    END = 7
 
 
 class MefControl:
@@ -18,12 +17,14 @@ class MefControl:
         self.car = car
         self.state = State.START
         self.next_state = State.START
+        self.angle_rotation_wheels = 0
 
         self.MAX_SPEED = 200
         self.MAX_ACCELERATION = 500
 
         self.THRESHOLD_COLOR_SENSOR = 30
-        self.ANGLE_ROTATION_WHEELS = 2
+        self.ANGLE_SMALL_ROTATION = 2
+        self.ANGLE_BIG_ROTATION = 25
 
     def update(self):
         self.setNextState()
@@ -36,7 +37,7 @@ class MefControl:
     def setNextState(self):
         print(f"state : {self.state}")
         print(f"self.car.wheel_angle : {self.car.wheel_angle}")
-        if self.check_color_sensor() == 3:
+        if self.check_color_sensor() == 2:
             self.next_state = State.END
             return
         if self.state == State.START:
@@ -47,25 +48,12 @@ class MefControl:
             elif self.car.get_speed() >= self.MAX_SPEED:
                 self.next_state = State.MAX_SPEED
             elif self.check_color_sensor() == 1:
-                self.next_state = State.TURN_LEFT
-            elif self.check_color_sensor() == 2:
-                self.next_state = State.TURN_RIGHT
+                self.next_state = State.TURN
             else:
                 self.next_state = State.ACCELERATION
-        elif self.state == State.TURN_LEFT:
+        elif self.state == State.TURN:
             if self.check_color_sensor() == 1:
-                self.next_state = State.TURN_LEFT
-            elif self.check_color_sensor() == 2:
-                self.next_state = State.TURN_RIGHT
-            elif self.car.get_speed() >= self.MAX_SPEED:
-                self.next_state = State.MAX_SPEED
-            else:
-                self.next_state = State.ACCELERATION
-        elif self.state == State.TURN_RIGHT:
-            if self.check_color_sensor() == 1:
-                self.next_state = State.TURN_LEFT
-            elif self.check_color_sensor() == 2:
-                self.next_state = State.TURN_RIGHT
+                self.next_state = State.TURN
             elif self.car.get_speed() >= self.MAX_SPEED:
                 self.next_state = State.MAX_SPEED
             else:
@@ -74,9 +62,7 @@ class MefControl:
             if self.check_distance_sensor() == 1:
                 self.next_state = State.STOP
             elif self.check_color_sensor() == 1:
-                self.next_state = State.TURN_LEFT
-            elif self.check_color_sensor() == 2:
-                self.next_state = State.TURN_RIGHT
+                self.next_state = State.TURN
             else:
                 self.next_state = State.MAX_SPEED
         elif self.state == State.STOP:
@@ -99,10 +85,19 @@ class MefControl:
             self.car.acceleration = [0, 0, 0]
         elif self.state == State.ACCELERATION:
             self.car.acceleration = utils.rotate_vector([0, 0, -self.MAX_ACCELERATION], self.car.get_rotation())
-        elif self.state == State.TURN_LEFT:
-            self.car.set_front_wheel_angle(self.ANGLE_ROTATION_WHEELS)
-        elif self.state == State.TURN_RIGHT:
-            self.car.set_front_wheel_angle(-self.ANGLE_ROTATION_WHEELS)
+        elif self.state == State.TURN:
+            color_sensor_values = self.car.color_sensor.read(self.car.get_location(), self.car.get_rotation()[2])
+            if color_sensor_values[0] < self.THRESHOLD_COLOR_SENSOR or color_sensor_values[1] < self.THRESHOLD_COLOR_SENSOR:
+                if color_sensor_values[0] < self.THRESHOLD_COLOR_SENSOR:
+                    self.angle_rotation_wheels = self.ANGLE_BIG_ROTATION
+                else:
+                    self.angle_rotation_wheels = self.ANGLE_SMALL_ROTATION
+            elif color_sensor_values[3] < self.THRESHOLD_COLOR_SENSOR or color_sensor_values[4] < self.THRESHOLD_COLOR_SENSOR:
+                if color_sensor_values[4] < self.THRESHOLD_COLOR_SENSOR:
+                    self.angle_rotation_wheels = -self.ANGLE_BIG_ROTATION
+                else:
+                    self.angle_rotation_wheels = -self.ANGLE_SMALL_ROTATION
+            self.car.set_front_wheel_angle(self.angle_rotation_wheels)
         elif self.state == State.MAX_SPEED:
             self.car.acceleration = [0, 0, 0]
         elif self.state == State.STOP:
@@ -118,9 +113,8 @@ class MefControl:
     def check_color_sensor(self):
         # 0 = sur la ligne
         # -1 = pas de ligne détectée
-        # 1 = trop à droite
-        # 2 = trop à gauche
-        # 3 = fin détectée (T)
+        # 1 = trop à droite ou à gauche
+        # 2 = fin détectée (T)
         color_sensor_values = self.car.color_sensor.read(self.car.get_location(), self.car.get_rotation()[2])
         print(f"color_sensor_values : {color_sensor_values}")
         nb = 0
@@ -128,24 +122,13 @@ class MefControl:
             if val < self.THRESHOLD_COLOR_SENSOR:
                 nb = nb + 1
         if nb == 5:
-            return 3
+            return 2
         elif nb == 0:
             return -1
         elif color_sensor_values[2] < self.THRESHOLD_COLOR_SENSOR:
             return 0
-        elif color_sensor_values[0] < self.THRESHOLD_COLOR_SENSOR or color_sensor_values[1] < self.THRESHOLD_COLOR_SENSOR:
-            if color_sensor_values[0] < self.THRESHOLD_COLOR_SENSOR:
-                self.ANGLE_ROTATION_WHEELS = 25
-            else:
-                self.ANGLE_ROTATION_WHEELS = 2
+        else:
             return 1
-        elif color_sensor_values[3] < self.THRESHOLD_COLOR_SENSOR or color_sensor_values[4] < self.THRESHOLD_COLOR_SENSOR:
-            if color_sensor_values[4] < self.THRESHOLD_COLOR_SENSOR:
-                self.ANGLE_ROTATION_WHEELS = 25
-            else:
-                self.ANGLE_ROTATION_WHEELS = 2
-            return 2
-        return 0
 
     def check_distance_sensor(self):
         # 0 = pas d'objet
